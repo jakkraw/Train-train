d0be2d559f710655da7e395b425d98eaae999a84
jakkraw <krawczykjakub100@gmail.com>
2018-10-29 01:21:36 +0100

    Added free parallax effect, basic train, script to move the train on any touch

---
+++ train-train/Assets/MoveTrain.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrain : MonoBehaviour {
+
+    private float speed = 0;
+    public float acceleration_speed = 8;
+    public float decelleration_speed = 7;
+    public FreeParallax parallax;
+    public Transform train;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.touchCount > 0)
+        {
+            speed += acceleration_speed * Time.deltaTime;
+        }
+        else
+        {
+            speed = Mathf.Max(0, speed - decelleration_speed * Time.deltaTime);
+        }
+      
+
+        if (parallax != null)
+        {
+            parallax.Speed = -speed;
+        }
+
+    }
+}
