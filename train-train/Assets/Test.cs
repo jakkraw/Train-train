4b77a35f37b4508a3ce826201be4571c8f5f07d3
jakkraw <krawczykjakub100@gmail.com>
2018-11-11 05:20:03 +0100

    Add passenger on click test

--- train-train/Assets/Test.cs
+++ train-train/Assets/Test.cs
@@ -19,5 +19,38 @@ public class Test : MonoBehaviour {
         if (Input.GetKey(KeyCode.DownArrow)){
             train.BackSeat.Remove();
         }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click, casting ray.");
+            CastRay();
+        }
+
     }
+
+    void CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            Debug.Log("Hit object: " + hit.collider.gameObject.name);
+
+            var el = hit.collider.gameObject.GetComponent<Seat>();
+            if (el)
+            {
+                if (el.isEmpty())
+                {
+                    el.Place(Instantiate(Resources.Load<GameObject>("Passenger")));
+                }
+                else
+                {
+                    el.Remove();
+                }
+            }
+
+        }
+    }
+
 }
